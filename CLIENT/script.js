33237143cf8111b32a3dfa794ea76c96863df5e8
husey <furaknzelik@gmail.com>
2024-04-20 15:19:29 +0200

    just error with connecting the server.js

--- CLIENT/script.js
+++ CLIENT/script.js
@@ -1,12 +1,17 @@
 const prompt = document.getElementById('prompt');
-const location = document.getElementById('location');
+// const location = document.getElementById('location');
 const typeTrip = document.getElementById('typeTrip');
 const replyField = document.getElementById('replyField');
 const sendButton = document.getElementById('send-button');
 const replyUser = document.getElementById('replyUser');
 const container3 = document.getElementById('container3');
+const suggestions = document.getElementById('suggestions');
 
 
+sendButton.addEventListener('click', retrieveMessage);
+
+
+let messageCounter = parseInt(localStorage.getItem('messageCounter')) || 0;
 
 
 
@@ -18,47 +23,79 @@ const container3 = document.getElementById('container3');
         }
     });
 
+document.addEventListener('DOMContentLoaded', async () => {
+    try {
+        await suggestionsReply();
+    }catch (e) {
+        console.error('does not work', e)
+    }
+});
+
     async function retrieveMessage() {
         container3.style.display = 'none';
         sendButton.style.display = 'none';
         location.style.display = 'none';
 
         try{
-            const responsBot = await sendChatMessage(
+            const responsBot = await sendMessage(
                 prompt.value,
                 location.value,
                 replyUser.value,
                 typeTrip.value,
             );
 
+            messageCounter++;
+            localStorage.setItem('messageCounter', messageCounter.toString());
+
+
+            if (messageCounter > 1) {
+                // Display AI response
+                displayMessage(responsBot, 'ai');
+
+                // Display user reply
+                displayMessage(replyUser.value, 'user');
+            } else {
+                // Display only AI response for the first message
+                displayMessage(responsBot, 'ai');
+            }
+
             prompt.value = '';
             location.value = '';
             replyUser.value = '';
             typeTrip.value = '';
 
-        };
+            replyField.style.display = 'block';
+            container3.style.display = 'block';
+            sendButton.style.display = 'inline-block';
+            sendButton.disabled = false;
+
+        } catch (e){
+            console.error('error message:', e.message);
+
+            suggestions.style.display = 'none';
+            sendButton.style.display = 'inline-block';
+            sendButton.disabled = false;
+            prompt.disabled = false;
+            location.disabled = false;
+            typeTrip.disabled = false;
+            replyField.disabled = false;
+
+
+        }
 
     }
 
-    document.addEventListener('DOMContentLoaded', async () => {
-        try {
-            await suggestionsReply();
-        }catch (e) {
-            console.error('does not work', e)
-        }
-    });
 
 
-
-async function sendChatMessage(message, gender, userLocation, userReply) {
+async function sendMessage(message,location, typeTrip, replyUser) {
     try {
-        const userPrompt = `message: ${message}, gender: ${gender}, userLocation: ${userLocation}, reply: ${userReply}`;
-        const response = await fetch('http://localhost:8001/chat', {
+        const inputPrompt = `message: ${message}, location: ${location}, reply: ${replyUser}, trip type: ${typeTrip}`;
+        const response = await fetch('http://localhost:8080/chatting', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify({userPrompt: userPrompt}),
+            body: JSON.stringify({inputPrompt: inputPrompt}),
         });
         if (!response.ok) {
             throw new Error(`HTTP error! Status: ${response.status}`);
@@ -81,3 +118,51 @@ async function sendChatMessage(message, gender, userLocation, userReply) {
 }
 
 
+function displayMessage(content, messageType) {
+    if (typeof content === 'string') {
+        content = content.replace(/^"(.*)"$/, '$1');
+
+        const messageContainer = generateMessageField(content, messageType);
+        appendToResponseContainer(messageContainer, container3.id, messageType);
+    } else {
+        console.error(`Invalid ${messageType} content:`, content);
+    }
+}
+
+
+    function generateMessageField (content,messageType){
+            const messageField = document.createElement('div');
+            messageField.classList.add('response-grid', 'col-span-3', 'mb-2', 'flex');
+
+            const messageIcon = document.createElement('i');
+            messageIcon.classList.add('fas', messageType === 'ai' ? 'fa-cat' : 'fa-user', 'text-black');
+            messageField.appendChild(messageIcon);
+
+            const messageText = document.createElement('p');
+            messageText.classList.add('typewriter', 'pl-4');
+            messageText.textContent = content;
+            messageField.appendChild(messageText);
+
+            messageField.classList.add(messageType === 'ai' ? 'ai-message' : 'user-message');
+
+            return messageField;
+
+    }
+
+
+
+    function addResponseField (messageField, containerId, messageType){
+
+        const responseField = document.getElementById(containerId);
+        if (responseField) {
+            const messageOrganiser = document.createElement('div');
+            messageOrganiser.classList.add('response-field');
+
+            messageOrganiser.classList.add(messageType === 'ai' ? 'ai-message' : 'user-message' );
+
+            messageOrganiser.appendChild(messageField);
+
+            responseField.insertBefore(messageOrganiser, responseField.firstChild);
+        }
+
+    }
--- SERVER/server.js
+++ SERVER/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import { ChatOpenAI } from "@langchain/openai";
 import cors from 'cors'
+import {ChatPromptTemplate as promptTemplate} from "@langchain/core/prompts";
 
 const app = express()
 const router = express.Router();
@@ -9,19 +10,65 @@ app.use(express.json());
 app.use(cors());
 app.use('/', router);
 
-const model = new ChatOpenAI({
-    azureOpenAIApiKey: process.env.AZURE_OPENAI_API_KEY,
-    azureOpenAIApiVersion: process.env.OPENAI_API_VERSION,
-    azureOpenAIApiInstanceName: process.env.INSTANCE_NAME,
-    azureOpenAIApiDeploymentName: process.env.ENGINE_NAME,
-});
+
+
 
 router.post('/chatting', async(req, res) =>{
     try{
-        const {query} = req.body;
-        console.log("Received query:", query); //  debugging
-        const response = await model.invoke(query);
-        res.json({response});
+      const promptUser = req.body.inputPrompt;
+      let locationUser = 'Amsterdam';
+      const typevacation = req.body.typeTrip;
+      console.log("User input:", promptUser);
+
+      //weather api
+
+        // const weatherApiKey = process.env.WEATHER_API_KEY;
+        // const weatherApiUrl = `https://api.weatherapi.com/v1/current.json?key=${weatherApiKey}&q=${userLocation}`;
+        // const weatherResponse = await axios.get(weatherApiUrl);
+
+        // if (weatherResponse.status !== 200) {
+        //     throw new Error(`Weather API request failed with status: ${weatherResponse.status}`);
+        // }
+        //
+        // const temperature = weatherResponse.data.current.temp_c;
+        // const weatherDescription = weatherResponse.data.current.condition.text;
+
+
+
+        const model = new ChatOpenAI({
+            azureOpenAIApiKey: process.env.AZURE_OPENAI_API_KEY,
+            azureOpenAIApiVersion: process.env.OPENAI_API_VERSION,
+            azureOpenAIApiInstanceName: process.env.INSTANCE_NAME,
+            azureOpenAIApiDeploymentName: process.env.ENGINE_NAME,
+            temperature: 0.2,
+        });
+
+
+        const promptEngineering = promptTemplate.fromTemplate(
+            `
+**Welcome! I'm your friendly vacation planning assistant. Let's craft the perfect getaway for you.**
+
+To get started, tell me a bit about yourself and your dream vacation:
+
+* **Tell me about yourself:** ${promptUser} (e.g., Are you traveling solo, with family, or with friends? What are your interests?)
+* **Where are you dreaming of going?** ${locationUser} (e.g., Do you have a specific destination in mind, or are you open to suggestions?)
+* **What kind of experience are you seeking?** ${typevacation} (e.g., Are you looking for relaxation, adventure, cultural immersion, or a combination?)
+
+**Feel free to share any additional details that would help me personalize your vacation plan.**
+
+**For example:**
+
+* Budget range
+* Preferred travel dates
+* Activities you're interested in
+* Any specific interests or needs (e.g., accessibility considerations, dietary restrictions)
+
+The more information you provide, the better I can tailor your vacation to your desires.
+
+**Let's get started on planning your unforgettable adventure!**
+`
+        );
+
     }
     catch(error){
         console.error("Error processing chat query:", error); //debugging
@@ -31,6 +78,7 @@ router.post('/chatting', async(req, res) =>{
 
 
 // Start the server
-app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`);
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
